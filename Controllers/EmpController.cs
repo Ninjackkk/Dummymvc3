a2897924c3d47d67545f1d031164789cdaea37cb
Lenovo <Lenovo@DESKTOP-KIPHLHP>
2024-08-09 17:09:49 +0530

    Add project files.

---
+++ Controllers/EmpController.cs
@@ -0,0 +1,42 @@
+﻿using Dummymvc3.Data;
+using Dummymvc3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dummymvc3.Controllers
+{
+    public class EmpController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public EmpController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddEmp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmp(Emp e)
+        {
+            db.emps.Add(e);
+            db.SaveChanges();
+            TempData["msg"] = "Emp Added Successfully";
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult ViewData()
+        {
+            var obj=db.emps.ToList();
+            return View(obj);
+        }
+    }
+}
---
+++ Models/Emp.cs
@@ -0,0 +1,11 @@
+﻿namespace Dummymvc3.Models
+{
+    public class Emp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public double Salary { get; set; }
+    }
+}
